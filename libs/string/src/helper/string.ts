2010f60c1e15f43bd036ae68eb2d9992fc38f2e9
Bérenger <berenger@arnaud.work>
2024-02-29 21:54:49 +0100

    feat(String): :sparkles: Add errorToString

--- libs/string/src/helper/string.ts
+++ libs/string/src/helper/string.ts
@@ -3,6 +3,8 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+// -- labelize -----------------------------------------------------------------
+
 /**
  * Transform string to lowercase with capitalized first letters and with spaces between words
  *
@@ -12,5 +14,33 @@ export function labelize(str: string): string {
   return str
     .split(/[-_ ]+/)
     .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ');
+    .join(" ");
+}
+
+// -- errorToString ------------------------------------------------------------
+
+/**
+ * Stringify an error.
+ *
+ * Will try to extract:
+ *  - .error.errorMessage
+ *  - .error
+ *  - .message
+ *  - <stringify>
+ *
+ * @param error any error
+ * @returns The stringified error
+ */
+export function errorToString(error: any): string {
+  if (typeof error === "string") {
+    return error;
+  } else if (error?.error?.errorMessage) {
+    return error.error.errorMessage;
+  } else if ("error" in error) {
+    return errorToString(error.error);
+  } else if (error instanceof Error || "message" in error) {
+    return error.message;
+  } else {
+    return JSON.stringify(error);
+  }
 }
